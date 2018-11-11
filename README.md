7e8a9fa6cc23f923b69b4fb060ac202b4a92e1d9
Ciro Souza <ciro.mies@gmail.com>
2018-11-11 23:20:00 +0000

    Fisrt commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Zoly - GA Refunds
+
+## Installation
+```
+$ git clone https://github.com/lucida-brasil/ga-refunds.git
+$ cd ga-refunds
+$ python3 -mvenv env
+$ . env/bin/activate
+$ python -mpip install -r requirements.txt
+$ cp .env.example .env
+```
+
+**Edit .env to set appropriate values to your environment**
+
+## Usage
+Create a csv file with data to be processed according to the following template
+| ga:transactionId | ga:productSku | ga:quantityRefunded |
+|------------------|---------------|---------------------|
+| 123456           | ABC123        | 1                   |
+| 123457           | ABC123        | 1                   |
+
+A data.csv.example file is provided as an example.
+Send the data to Google Analytics
+```
+$ python refund.py -f filnename.csv
+```
+
+# Info
+```
+usage: refund.py [-h] -f FILENAME [-n] [-d]
+
+Process GA refund data.
+
+optional arguments:
+  -h, --help   show this help message and exit
+  -f FILENAME  Name of the csv file to be processed.
+  -n           Dry Run.
+  -d           Debug.
+```
---
+++ refund.py
@@ -0,0 +1,50 @@
+import argparse, os, csv, requests
+from types import SimpleNamespace
+from uuid import uuid1
+from dotenv import load_dotenv
+from pprint import pprint
+
+load_dotenv()
+url = 'https://www.google-analytics.com/collect'
+
+def prepare_payload(transaction_id, product_sku, product_qty):
+	payload = {
+		'v': 1,
+		't': 'event',
+		'ni': 1,
+		'cid': str(uuid1()),
+		'tid': os.getenv('PROPERTY'),
+		'dh': os.getenv('HOSTNAME'),
+		'cd2': os.getenv('GTM'),
+		'ec': os.getenv('EVENT_CATEGORY'),
+		'ea': os.getenv('EVENT_ACTION'),
+		'el': os.getenv('EVENT_LABEL'),
+		'pa': 'refund',
+		'ti': transaction_id,
+		'pr1id': product_sku,
+		'pr1qt': int(product_qty),
+	}
+
+	return payload
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Process GA refund data.')
+	parser.add_argument('-f', required=True, dest='filename', help='Name of the csv file to be processed.')
+	parser.add_argument('-n', required=False, dest='dryrun', action='store_true', help='Dry Run.')
+	parser.add_argument('-d', required=False, dest='debug', action='store_true', help='Debug.')
+	args = parser.parse_args()
+
+	with open(args.filename) as csvfile:
+		reader = csv.reader(csvfile)
+		next(reader, None)
+
+		for row in reader:
+			payload = prepare_payload(*row)
+			if (args.debug):
+				pprint(payload)
+
+			a = SimpleNamespace(status_code = 200)
+			if (not args.dryrun):
+				a = requests.post(url, data=payload)
+
+			print('Sent refund to transaction %s, SKU %s, quantity %s - status: %s' % (*row, a.status_code))
